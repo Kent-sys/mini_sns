30ea22c926fc215282cfd703932a3db89f9cab68
Kent <dbsxk133@yahoo.co.jp>
2021-06-28 19:09:42 +0900

    画像アップロード処理の修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,38 +67,38 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        // //引数 $data から name='img_name'を取得(アップロードするファイル情報)
-        // $imageFile = $data['img_name'];
+        //引数 $data から name='img_name'を取得(アップロードするファイル情報)
+        $imageFile = $data['img_name'];
 
-        // //$imageFileからファイル名を取得(拡張子あり)
-        // $filenameWithExt = $imageFile->getClientOriginalName();
+        //$imageFileからファイル名を取得(拡張子あり)
+        $filenameWithExt = $imageFile->getClientOriginalName();
 
-        // //拡張子を除いたファイル名を取得
-        // $fileName = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+        //拡張子を除いたファイル名を取得
+        $fileName = pathinfo($filenameWithExt, PATHINFO_FILENAME);
 
-        // //拡張子を取得
-        // $extension = $imageFile->getClientOriginalExtension();
+        //拡張子を取得
+        $extension = $imageFile->getClientOriginalExtension();
 
-        // // ファイル名_時間_拡張子 として設定
-        // $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+        // ファイル名_時間_拡張子 として設定
+        $fileNameToStore = $fileName.'_'.time().'.'.$extension;
 
-        // //画像ファイル取得
-        // $fileData = file_get_contents($imageFile->getRealPath());
+        //画像ファイル取得
+        $fileData = file_get_contents($imageFile->getRealPath());
 
-        // $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
+        $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
 
 
-        // //画像アップロード(Imageクラス makeメソッドを使用)
-        // $image = Image::make($data_url);
+        //画像アップロード(Imageクラス makeメソッドを使用)
+        $image = Image::make($data_url);
 
-        // //画像を横100pxにリサイズし保存
-        // $image->resize(100, null, function($constraint){$constraint->aspectRatio();})->save(storage_path() . '/app/public/images/' . $fileNameToStore );
+        //画像を横100pxにリサイズし保存
+        $image->resize(100, null, function($constraint){$constraint->aspectRatio();})->save(storage_path() . '/app/public/images/' . $fileNameToStore );
         
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            //'img_name' => $fileNameToStore,
+            'img_name' => $fileNameToStore,
         ]);
     }
 }
